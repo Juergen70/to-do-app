1f56a40f09e031f6d65501039a5856887bc4956b
Juergen70 <juergen240@googlemail.com>
2025-03-05 12:54:12 +0100

    Datenbank erstellt, POSTrequest zum Item hinzufügen

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,29 @@
 const express = require('express');
-
+const sqlite3 = require('sqlite3');
+const bodyParser = require("body-parser");
 
 const app = express();
 
 // TODO: Verbinde eine Datenbank dazu
+
+const db = new sqlite3.Database('./tasks.db');
+
+app.use(bodyParser.json()); // Middelware
+
+db.run('CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, completed BOOLEAN DEFAULT 0)');
+
+
 // TODO: Schreibe request/respons
 
 app.get('/', (req, res)=> {
     res.send('request received');
 })
+
+app.post('/add', (req, res)=>{
+    db.run(INSERT INTO tasks (titel) VALUES(?), [req.body.titel] function () {
+    res.json({tag: "Mittwoch", bald_wirds: "Mittagspause"});
+    });
+});
 
 app.listen(3050,"localhost",()=>{
     console.log("bald ist Mittagspause")
