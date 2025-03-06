78deada452ec0930b3a8ec3a6015c47a60c338a0
Juergen70 <juergen240@googlemail.com>
2025-03-06 11:07:09 +0100

    visuelle frontend erstellt

--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,48 @@
 const express = require('express');
 const sqlite3 = require('sqlite3');
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 
 const app = express();
 
-// TODO: Verbinde eine Datenbank dazu
+//TODO: Verbinde eine Datenbank dazu
 
 const db = new sqlite3.Database('./tasks.db');
 
-app.use(bodyParser.json()); // Middelware
+app.use(bodyParser.json());     // Middleware (wie ein Übersetzer)
+
 
 db.run('CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, completed BOOLEAN DEFAULT 0)');
 
 
-// TODO: Schreibe request/respons
+//TODO: Schreibe requests/responses
 
-app.get('/', (req, res)=> {
+
+app.get('/', (req, res) => {
     res.send('request received');
-})
+});
 
-//Wenn ein neues Item hinzugefügt werden soll
-// soll Nodejs server dieses so behandeln
-app.post('/add', (req, res)=>{
-    db.run(INSERT INTO tasks (titel) VALUES(?), [req.body.titel] function() {
-      res.json({tag: "Mittwoch", bald_wirds: "Mittagspause"});
+app.get('/ralf', (req, res) => {
+    res.send('vielen Dank Ralf');
+});
+
+// Wenn ein neues Item hinzugefügt werden soll, soll NodeJS Server diesen Request so behandeln:
+app.post('/add', (req, res) => {
+    db.run('INSERT INTO tasks (title) VALUES (?)', [req.body.title], function () {
+        res.json({tag: "Mittwoch", bald_wirds: "Mittagspause"});
     });
 });
 
-// Liste mir alle existierenden Items
-// Hier sollten nur alle Items als JSON im Response geschrieben werden 
-app.get('/Liste_abrufen',(reg, res)=>{
-    db.all('SELECT * FROM tasks', function(err,rows ){
+
+// Liste mir alle existierende Items
+// hier sollte nur alle Items als JSON im Response geschrieben werden
+app.get('/liste_abrufen', (req, res) => {
+    db.all('SELECT * FROM tasks', function (err, rows){
         res.json(rows);
     })
-})
+});
 
-app.listen(3050,"localhost",()=>{
-    console.log("bald ist Mittagspause")
-})
+
+
+app.listen(3050, "localhost", () => {
+    console.log("bald wird es Mittagspause")
+});
